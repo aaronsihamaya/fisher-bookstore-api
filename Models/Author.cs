205db102b4b3c64ccfe10171c7501bdeece55167
Aaron Sihamaya <aaron.sihamaya@gmail.com>
2018-02-11 20:47:04 -0500

    API

---
+++ Models/Author.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Fisher.Bookstore.Api.Models
+{
+    public class Author
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string author { get; set; }
+
+    }
+}
